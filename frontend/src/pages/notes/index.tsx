d4ad2bf305c13c9811fc06e73a6a9162547d1f39
Pawel <roreth@wp.pl>
2022-05-25 08:13:57 +0200

    add-mui-and-base-table

--- frontend/src/pages/notes/index.tsx
+++ frontend/src/pages/notes/index.tsx
@@ -1,7 +1,30 @@
 import type { Component } from 'solid-js';
+import Table from '@suid/material/Table';
+import TableBody from '@suid/material/TableBody';
+import TableCell from '@suid/material/TableCell';
+import TableContainer from '@suid/material/TableContainer';
+import TableHead from '@suid/material/TableHead';
+import TableRow from '@suid/material/TableRow';
+
+const rows = [
+  { title: 'ehe', tags: ['js', 'frontend'] },
+  { title: 'nope', tags: ['node', 'backend'] },
+];
 
 const Notes: Component = () => {
-  return <div>all notes</div>;
+  return (
+    <div>
+      all notes
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell>Dessert (100g serving)</TableCell>
+            <TableCell align="right">Calories</TableCell>
+          </TableRow>
+        </TableHead>
+      </Table>
+    </div>
+  );
 };
 
 export default Notes;
