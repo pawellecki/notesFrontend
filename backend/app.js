1b3f7dda313064013df99cc5abe4f8a19701c8cd
Pawel <roreth@wp.pl>
2022-06-16 12:01:01 +0200

    B-cors-setHeader

--- backend/app.js
+++ backend/app.js
@@ -11,6 +11,20 @@ const app = express();
 
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PUT, PATCH, DELETE'
+  );
+
+  next();
+});
+
 app.use('/api/notes', notesRoutes);
 app.use('/api/users', usersRoutes);
 
