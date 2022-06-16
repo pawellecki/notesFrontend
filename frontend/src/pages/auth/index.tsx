b2c824b0dc0841acf3a1c32b753b0c4bb905eb6b
Pawel <roreth@wp.pl>
2022-06-16 12:08:43 +0200

    F-felte-form-yup-login

---
+++ frontend/src/pages/auth/index.tsx
@@ -0,0 +1,68 @@
+import type { Component } from 'solid-js';
+import { createSignal } from 'solid-js';
+import { createForm } from '@felte/solid';
+import Input from '../../components/Form/Input/Input';
+import Button from '../../components/Button/Button';
+
+const isLoggedIn = false;
+
+const Auth: Component = () => {
+  const [isLoading, setIsLoading] = createSignal(false);
+
+  // @ts-ignore
+  const { form } = createForm({
+    onSubmit: async (values) => {
+      console.log('sumbit values', values);
+      if (isLoggedIn) {
+        return;
+      } else {
+        try {
+          console.log('try', values);
+          setIsLoading(true);
+
+          const response = await fetch(
+            'http://localhost:5000/api/users/signup',
+            {
+              method: 'POST',
+              headers: {
+                'Content-Type': 'application/json',
+              },
+              body: JSON.stringify({
+                name: values.name,
+                email: values.email,
+                password: values.password,
+              }),
+            }
+          );
+
+          const responseData = await response.json();
+          console.log('responseDataa', responseData);
+        } catch (err) {
+          console.log('log in err', err);
+        }
+      }
+
+      setIsLoading(true);
+    },
+  });
+
+  return (
+    <div style={{ display: 'flex', 'justify-content': 'center' }}>
+      <form
+        use:form
+        style={{
+          display: 'flex',
+          'flex-direction': 'column',
+          width: '300px',
+        }}
+      >
+        <Input label="Name" name="name" />
+        <Input label="Email" name="email" />
+        <Input label="Password" name="password" type="password" />
+        <Button type="submit">Sign In</Button>
+      </form>
+    </div>
+  );
+};
+
+export default Auth;
