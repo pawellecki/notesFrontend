a18db6204743e22a45f74198a60b035635fc7682
Pawel <roreth@wp.pl>
2022-06-16 19:10:27 +0200

    F-routing-global-store

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,4 @@
-import type { Component } from 'solid-js';
-import { createSignal } from 'solid-js';
+import { Component } from 'solid-js';
 import { Routes, Route } from 'solid-app-router';
 import { Toaster } from 'solid-toast';
 import styles from './App.module.css';
@@ -8,48 +7,43 @@ import Notes from './pages/notes';
 import NewNote from './pages/newNote';
 import UpdateNote from './pages/updateNote';
 import Auth from './pages/auth';
+import { isLoggedIn } from '../globalStore';
 
-const App: Component = () => {
-  const [isLoggedIn, setIsLoggedIn] = createSignal(false);
+const App: Component = () => (
+  <div class={styles.App}>
+    <div>heeeeeeeeader</div>
+    <div>sidebar</div>
+    <Toaster />
 
-  const login = () => setIsLoggedIn(true);
-
-  const logout = () => setIsLoggedIn(false);
-  console.log('is in', isLoggedIn());
-  return (
-    <div class={styles.App}>
-      <div>heeeeeeeeader</div>
-      <div>sidebar</div>
-      <Toaster />
+    {isLoggedIn() && (
       <Routes>
-        {isLoggedIn() ? (
-          <>
-            {/* <Route path="/">
+        <Route path="/" element={<Notes />} />
+        <Route path="/tinder" element={<Tinder />} />
+        <Route path="/:userId/notes" element={<UpdateNote />} />
+        <Route path="/notes/new" element={<NewNote />} />
+        <Route path="/notes/:id" element={<UpdateNote />} />
+      </Routes>
+    )}
+
+    {!isLoggedIn() && (
+      <Routes>
+        <Route path="/" element={<Auth />} />
+      </Routes>
+    )}
+
+    {/* <Route path="/">
   <Users /> user/pages/users
 </Route> */}
-            {/* <Route path="/:userId/places">
+    {/* <Route path="/:userId/places">
   <UserPlaces />
 </Route> */}
-            {/* <Route path="/places/new">
+    {/* <Route path="/places/new">
   <NewPlace />
 </Route> */}
-            {/* <Route path="/places/:placeId">
+    {/* <Route path="/places/:placeId">
   <UpdatePlace />
 </Route> */}
-            <Route path="/" element={<Notes />} />
-            <Route path="/tinder" element={<Tinder />} />
-            <Route path="/:userId/notes" element={<UpdateNote />} />
-            <Route path="/notes/new" element={<NewNote />} />
-            <Route path="/notes/:id" element={<UpdateNote />} />
-          </>
-        ) : (
-          <>
-            <Route path="/" element={<Auth />} />
-          </>
-        )}
-      </Routes>
-    </div>
-  );
-};
+  </div>
+);
 
 export default App;
